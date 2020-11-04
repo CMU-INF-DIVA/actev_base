5ea8f54f4828078d87050a67d1bb71869675f7b0
Lijun Yu <lijun@lj-y.com>
2020-11-04 16:53:43 -0500

    add git to dep

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ cd $(cd $(dirname $0); pwd)
 CONDA_DIR=$(dirname $CONDA_EXE)/..
 source $CONDA_DIR/etc/profile.d/conda.sh
 
-sudo apt install -y gcc g++ pkg-config libgl1-mesa-dev
+sudo apt install -y git gcc g++ pkg-config libgl1-mesa-dev
 CC="cc -mavx2" conda env create -f environment.yml -n $name
 
 conda activate $name
