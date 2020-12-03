57792010ed2990ff6b9a22962faeacbd8ba6be52
Lijun Yu <lijun@lj-y.com>
2020-12-03 13:00:01 -0500

    update conda

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ name=${1:-actev}
 cd $(cd $(dirname $0); pwd)
 CONDA_DIR=$(dirname $CONDA_EXE)/..
 source $CONDA_DIR/etc/profile.d/conda.sh
+conda update -n base -c defaults conda
 
 sudo apt install -y git gcc g++ pkg-config libgl1-mesa-dev
 CC="cc -mavx2" conda env create -f environment.yml -n $name
