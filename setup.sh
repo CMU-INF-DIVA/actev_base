4457cb3a72df9d4aeb36c403ec7d7fea6cb7a6ac
Lijun Yu <lijun@lj-y.com>
2021-11-05 11:55:32 -0400

    add selenium

--- setup.sh
+++ setup.sh
@@ -4,12 +4,15 @@ set -e
 
 name=${1:-actev}
 
+if $(id | grep sudo &> /dev/null); then
+    sudo apt install -y git gcc g++ pkg-config libgl1-mesa-dev
+fi
+
 cd $(cd $(dirname $0); pwd)
 CONDA_DIR=$(dirname $CONDA_EXE)/..
 source $CONDA_DIR/etc/profile.d/conda.sh
-conda update -n base -c defaults conda
+conda update -n base -c defaults conda -y
 
-sudo apt install -y git gcc g++ pkg-config libgl1-mesa-dev
 CC="cc -mavx2" conda env create -f environment.yml -n $name
 
 conda activate $name
