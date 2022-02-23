2791626dc143c16877f392f0a25d84715f38c264
Lijun Yu <lijun@lj-y.com>
2022-02-23 13:50:51 -0500

    set ffmpeg and av version

--- update_ffmpeg.sh
+++ update_ffmpeg.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-conda install "ffmpeg>=4.3.1" -c conda-forge -y
+conda install "ffmpeg==4.3.1=h03821db_2" -c conda-forge -y
 conda env config vars set FFMPEG_BINARY=auto-detect
 export PKG_CONFIG_PATH="$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
 pip uninstall -y av
-pip install av --no-binary av
+pip install "av<8.0.3" --no-binary av
